e35ee682c614c580ecc820cd8baa2fe265d189ba
Mingjie Li <limingjie@users.noreply.github.com>
2017-07-03 12:11:58 +0800

    clean log files

--- admin/zerodisk.sh
+++ admin/zerodisk.sh
@@ -3,6 +3,9 @@
 # Copyright (C) 2015 by Mingjie Li, <limingjie@outlook.com>
 # https://github.com/limingjie/dotfiles
 
+# clean log files (https://unix.stackexchange.com/questions/27649/cleaning-log-files-under-linux)
+find /var/log -type f -exec /bin/cp /dev/null {} \;
+
 # sda
 cd / ; cat /dev/zero > zero.fill ; sync ; sleep 1 ; sync ; rm -f zero.fill
 
