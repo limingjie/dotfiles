43b3ce31d0a7cc60d226120fe4b066970e327a1d
Mingjie Li <limingjie@outlook.com>
2024-06-26 12:24:55 -0700

    Add zjstatus plugin in dotfile

--- script/install.sh
+++ script/install.sh
@@ -40,10 +40,11 @@ else
   relink_sudo ${dotfilesroot}/etc/zshenv            /etc/zsh/zshenv
 fi
 
-relink      ${dotfilesroot}/alacritty/alacritty.yml ~/.config/alacritty.yml
-relink      ${dotfilesroot}/starship/starship.toml  ~/.config/starship.toml
-relink      ${dotfilesroot}/zsh/zshrc               ~/.config/zsh/.zshrc
-relink      ${dotfilesroot}/nvim/init.vim           ~/.config/nvim/init.vim
-relink      ${dotfilesroot}/kitty/kitty.conf        ~/.config/kitty/kitty.conf
-relink      ${dotfilesroot}/kitty/theme.conf        ~/.config/kitty/theme.conf
-relink      ${dotfilesroot}/zellij/default.kdl      ~/.config/zellij/layouts/default.kdl
+relink      ${dotfilesroot}/alacritty/alacritty.yml        ~/.config/alacritty.yml
+relink      ${dotfilesroot}/starship/starship.toml         ~/.config/starship.toml
+relink      ${dotfilesroot}/zsh/zshrc                      ~/.config/zsh/.zshrc
+relink      ${dotfilesroot}/nvim/init.vim                  ~/.config/nvim/init.vim
+relink      ${dotfilesroot}/kitty/kitty.conf               ~/.config/kitty/kitty.conf
+relink      ${dotfilesroot}/kitty/theme.conf               ~/.config/kitty/theme.conf
+relink      ${dotfilesroot}/zellij/default.kdl             ~/.config/zellij/layouts/default.kdl
+relink      ${dotfilesroot}/zellij/plugins/zjstatus.wasm   ~/.config/zellij/plugins/zjstatus.wasm
