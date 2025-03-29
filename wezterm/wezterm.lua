4ee8f66d885439cf1e3d987bcf58418411cd3876
Mingjie Li <limingjie@outlook.com>
2025-03-29 14:45:52 -0700

    wezterm - Maple Mono font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,18 @@ local config = wezterm.config_builder()
 -- Wezterm embeds the Nerd Font symbols by default.
 config.font_size = 16
 config.font = wezterm.font {
-    family = 'Iosevka Curly',
+    family = 'Maple Mono NF CN',
     weight = 'Light',
+    -- italic = true,
     harfbuzz_features = {
-        'kern', 'liga', 'clig', -- default values
-        'onum',                 -- enable old-style number
-        'frac',                 -- enable fractions
-        'cv01=3', 'cv03=1', 'cv04=9', 'cv07=7', 'cv10=16', 'cv20=11', 'cv23=11', 'cv35=14', 'cv56=7', 'cv60=14'
+        -- Iosevka
+        -- 'kern', 'liga', 'clig', -- default values
+        -- 'onum',                 -- enable old-style number
+        -- 'frac',                 -- enable fractions
+        -- 'cv01=3', 'cv03=1', 'cv04=9', 'cv07=7', 'cv10=16', 'cv20=11', 'cv23=11', 'cv35=14', 'cv56=7', 'cv60=14'
+
+        -- Maple Mono
+        'calt', 'cv01', 'cv03',
     }
 }
 config.freetype_load_target = "Light"
@@ -26,7 +31,10 @@ config.window_decorations = 'RESIZE'
 config.background = {
     {
         source = {
-            File = wezterm.config_dir .. '/wallpapers/5cm_morning.jpg',
+            File = wezterm.config_dir .. '/wallpapers/5cm_launch.jpg',
+            -- File = wezterm.config_dir .. '/wallpapers/5cm_morning.jpg',
+            -- File = wezterm.config_dir .. '/wallpapers/5cm_train_crossing.jpeg',
+            -- File = wezterm.config_dir .. '/wallpapers/starscape-kosmos.jpg',
         },
         horizontal_align = "Center",
         vertical_align = "Middle",
